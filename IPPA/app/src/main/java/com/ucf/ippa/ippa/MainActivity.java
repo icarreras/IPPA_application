6717623ec5aa29f33b1d8b1f904ef37eb30bd0de
icarreras <ivette.carreras@knights.ucf.edu>
2015-01-13 10:47:44 -0500

    Main Activity Setup
    Main activity for voice and instructions
    Instructions activity

---
+++ IPPA/app/src/main/java/com/ucf/ippa/ippa/MainActivity.java
@@ -0,0 +1,51 @@
+package com.ucf.ippa.ippa;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize GUI objects
+        final Button buttonVoiceCommand = (Button) findViewById(R.id.voice_command_button);
+        final TextView textViewConnectionStatus = (TextView) findViewById(R.id.connection_status);
+
+        String connectionStatus = "Not Connected";
+
+        // Set status
+        textViewConnectionStatus.setText(connectionStatus);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
